a1371a342366772f457476f9202af1cc0d63b465
Joe Cheatham <cheathamjoe@gmail.com>
2015-09-19 12:40:40 -0500

    added working stumble button

--- ext/src/js/popup.js
+++ ext/src/js/popup.js
@@ -1,3 +1,9 @@
-$('#git_btn').click(function(){
+
+	$('#git_btn').click(function(){
 	console.log('git');
+	chrome.tabs.getSelected(null, function(tabs) {
+	chrome.tabs.update(tabs.id, {
+        url: "https://github.com/dart-lang/html"
+    });
+});
 });
