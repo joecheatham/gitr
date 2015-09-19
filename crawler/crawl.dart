0c6cabe1e4eeb1df856f5411af269ba445d0cc71
Joe Cheatham <cheathamjoe@gmail.com>
2015-09-19 03:40:59 -0500

    crawler adds to mongo now

--- crawler/crawl.dart
+++ crawler/crawl.dart
@@ -1,61 +1,65 @@
 import 'dart:io';
+import 'dart:core';
 import 'dart:async';
 import 'package:html/parser.dart' show parse;
 import 'package:html/dom.dart';
-
-class Page {
-  String url;
-  String repo;
-  String user;
-  String desc;
-  Map<String, int> langs;
-}
-
+import 'package:mongo_dart/mongo_dart.dart';
 
 main() {
-  final String github = "https://github.com/";
-  String url = 'https://github.com/search?q=stars%3A%3E1&type=Repositories';
-  for (int i = 1; i <= 1; i++) {
-    getHtml(url).then((document) {
-      document.querySelectorAll('ul.repo-list').forEach((e) {
-        String title = e.querySelector('h3.repo-list-name > a').text.split('/')[0];
-        getHtml(github + title).then((user) {
-          user.querySelectorAll('h3.repo-list-name > a').forEach((a) {
-            scrape(github + title + '/' + a.text.trim());
-          });
+    String url = 'https://github.com/trending';
+    crawl(url);
+}
+
+crawl(String url) {
+  final String github = "https://github.com/trending?since=weekly";
+  int count = 0;
+  getHtml(url).then((document) {
+    document.querySelectorAll('ul.repo-list > li').forEach((e) {
+      String title =
+      e.querySelector('h3.repo-list-name > a > span.prefix').text;
+
+      getHtml(github + title).then((user) {
+        user.querySelectorAll('h3.repo-list-name > a').forEach((a) {
+          scrape(github + title + '/' + a.text.trim());
+          count++;
+          print(count.toString());
         });
       });
     });
-  }
+  });
 }
 
+
 scrape(String url) {
-  getHtml(url).then((document) {
-    // page title
-    print(url);
+  Db db = new Db("mongodb://admin:password@ds051523.mongolab.com:51523/gitr");
+  db.open().then((success) {
+    if (success) {
+      var collection = db.collection('repositories');
+      getHtml(url).then((document) {
+        var langs = new Map();
+        document.querySelectorAll('.repository-lang-stats-numbers > li').forEach((e) {
+          langs[e.querySelector('.lang').text] = double.parse(e.querySelector('.percent').text.split('%')[0]);
+        });
+        String desc = "";
+        if (document.querySelector('#readme > .markdown-body > p') != null)
+          desc = document.querySelector('#readme > .markdown-body > p').text;
 
-    //user
-    print(document.querySelector('.author > .url > span').text);
-    //repo
-    print(document.querySelector('.entry-title > strong').text);
-    //desc
-    print(document.querySelector('#readme > .markdown-body > p').text);
-    //langs
-    document.querySelectorAll('.repository-lang-stats-numbers > li').forEach((e) {
-      print(e.querySelector('.lang').text + ': ' +  e.querySelector('.percent').text);
-    });
-    //lines
+        collection.update(where.eq('url', url), modify.set('url', url), upsert: true);
+        collection.update(where.eq('url', url), modify.set('user', document.querySelector('.author > .url > span').text), upsert: true);
+        collection.update(where.eq('url', url), modify.set('repo', document.querySelector('.entry-title > strong').text), upsert: true);
+        collection.update(where.eq('url', url), modify.set('langs', langs), upsert: true);
+        collection.update(where.eq('url', url), modify.set('desc', desc), upsert: true);
+        collection.update(where.eq('url', url), modify.set('stars', int.parse(document.querySelector('a.social-count.js-social-count').text.trim().replaceAll(',',''))), upsert: true);
 
-    print('\n');
+        });
+    }
   });
 }
 
 /// fetch and parse the HTML from [url]
-Future<Document> getHtml(String url) =>
-    new HttpClient()
-      .getUrl(Uri.parse(url))
-      .then((req) => req.close())
-      .then((res) => res
-          .asyncExpand((bytes) => new Stream.fromIterable(bytes))
-          .toList())
-      .then((bytes) => parse(bytes, sourceUrl: url));
+Future<Document> getHtml(String url) => new HttpClient()
+.getUrl(Uri.parse(url))
+.then((req) => req.close())
+.then((res) =>
+  res.asyncExpand((bytes) => new Stream.fromIterable(bytes)).toList())
+.then((bytes) => parse(bytes, sourceUrl: url));
