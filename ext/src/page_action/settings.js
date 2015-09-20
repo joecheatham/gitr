0a56927082ab70e7cf75904f38d0d3a1716b423e
Joe Cheatham <cheathamjoe@gmail.com>
2015-09-20 07:57:29 -0500

    display user settings

--- ext/src/page_action/settings.js
+++ ext/src/page_action/settings.js
@@ -1,28 +1,28 @@
 
 
-function onReady(callback) {
-	var intervalID = window.setInterval(checkReady, 1000);
-	function checkReady() {
-		if (document.getElementsByTagName('body')[0] !== undefined) {
-			window.clearInterval(intervalID);
-			callback.call(this);
-		}
-	}
-}
+// function onReady(callback) {
+// 	var intervalID = window.setInterval(checkReady, 1000);
+// 	function checkReady() {
+// 		if (document.getElementsByTagName('body')[0] !== undefined) {
+// 			window.clearInterval(intervalID);
+// 			callback.call(this);
+// 		}
+// 	}
+// }
 
-function show(id, value) {
-	document.getElementById(id).style.display = value ? 'block' : 'none';
-}
+// function show(id, value) {
+// 	document.getElementById(id).style.display = value ? 'block' : 'none';
+// }
 
-onReady(function () {
-	show('page', true);
-	show('loading', false);
-});
+// onReady(function () {
+// 	show('page', true);
+// 	show('loading', false);
+// });
 
 
 
 $(document).ready(function() {
-
+	var languagess = Object.keys(allLangs);
 	function loadPrefs() {
 		chrome.storage.sync.get('languages', function(data) {
 		var langs = "";
@@ -36,25 +36,15 @@ $(document).ready(function() {
 				langs = "C,C++,Java,Javascript,Ruby";
 		}
 
-		//add to tagit here
-
-		});
-	}
-
-	function savePrefs() {
-		chrome.storage.sync.set({'languages': $('#mySingleField').val()}, function() {
-        });
-	}
-
-	var langs = Object.keys(allLangs);
-	$('#singleFieldTags').tagit({
-        availableTags: langs,
+		$('#mySingleField').val(langs);
+		$('#singleFieldTags').tagit({
+        availableTags: languagess,
         showAutocompleteOnFocus: true,
         singleField: true,
         singleFieldNode: $('#mySingleField'),
         beforeTagAdded: function(event, ui) {
-        	for (var i = 0; i < langs.length; i++) {
-				if (ui.tagLabel == langs[i]) {
+        	for (var i = 0; i < languagess.length; i++) {
+				if (ui.tagLabel == languagess[i]) {
 					return true;
 				} 
 			};
@@ -67,6 +57,18 @@ $(document).ready(function() {
     		savePrefs();
     	}
     });
+		});
+	}
+
+	loadPrefs();
+
+	function savePrefs() {
+		chrome.storage.sync.set({'languages': $('#mySingleField').val()}, function() {
+        });
+	}
+
+
+	
 
     $('#close').click(function() {
     	chrome.windows.getCurrent(function(data) {
