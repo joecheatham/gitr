8b3ad43a0fdde1d121e18a452f61cbc6430bc05f
Joe Cheatham <cheathamjoe@gmail.com>
2015-09-20 00:53:08 -0500

    use chrome sync storage

--- ext/src/js/popup.js
+++ ext/src/js/popup.js
@@ -1,21 +1,39 @@
 function getRandomIndex(max) {
-  return Math.floor(Math.random() * (max));
+	return Math.floor(Math.random() * (max));
 }
 
-$('#git_btn').click(function(){
-	console.log('git');
-	chrome.tabs.getSelected(null, function(tabs) {
-		$.ajax('http://localhost:8080/api/stumble/v1/google?languages=Matlab,Lua,Dart').success(function(data) {
-			var url = data[getRandomIndex(data.length)]['url'];
-			chrome.tabs.update(tabs.id, {
-				url: url
+$('#git_btn').click(function() {
+
+	chrome.storage.sync.get('languages', function(data) {
+		var langs = "";
+
+		if(chrome.runtime.lastError)
+			langs = "C,C++,Java,Javascript,Ruby";
+		else  {
+			if (typeof data === 'string' || data instanceof String)
+				langs = data;
+			else
+				langs = "C,C++,Java,Javascript,Ruby";
+		}
+
+
+
+			chrome.tabs.getSelected(null, function(tabs) {
+				$.ajax('http://localhost:8080/api/stumble/v1/google?languages=' + langs).success(function(data) {
+					var url = data[getRandomIndex(data.length)]['url'];
+					chrome.tabs.update(tabs.id, {
+						url: url
+					});
+				});
 			});
-		});
+
+
 	});
+
+	
 });
 
 $('#pref_icon').click(function() {
-	console.log('prefs');
 	chrome.tabs.getSelected(null, function(tabs) {
 		chrome.tabs.update(tabs.id, {
 			url: 'src/page_action/settings.html'
